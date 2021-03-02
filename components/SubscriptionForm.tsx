b1143d97c9476523b0db36319fbf5467969de2a3
Alan Christopher Alamillo Medina <realconect@gmail.com>
2021-03-02 02:51:07 -0600

    Added subscription and footer

---
+++ components/SubscriptionForm.tsx
@@ -0,0 +1,30 @@
+const SubscriptionForm = () => {
+  return (
+    <div className="text-center">
+      <form>
+        <p className="text-5xl font-bold leading-snug mb-8 bg-clip-text text-transparent bg-gradient-to-bl from-teal-900 to-yellow-300">
+          Amazing content
+          <br />
+          on your mailbox
+        </p>
+        <p className="w-1/2 mx-auto text-xl leading-8 mb-12">
+          Join my newsletter and get cool articles about Web Development, Design
+          and more cool stuff.
+        </p>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your e-mail here"
+          className="w-1/4 px-4 py-2 bg-gray-300 border border-gray-800 rounded-tl-md rounded-bl-md outline-none focus:outline-none focus:ring focus:border-pink-300 placeholder-gray-500"
+        />
+        <input
+          type="submit"
+          value="Sign up"
+          className="bg-gradient-to-bl from-pink-900 to-pink-300 font-bold px-4 py-2 rounded-tr-md rounded-br-md cursor-pointer focus:outline-none focus:ring focus:border-pink-300"
+        />
+      </form>
+    </div>
+  )
+}
+
+export default SubscriptionForm
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
 import Hero from '@/components/Hero'
 import LatestPosts from '@/components/LatestPosts'
+import SubscriptionForm from '@/components/SubscriptionForm'
+import Footer from '@/components/Footer'
 
 export default function Home() {
-  return <div>
-    <Hero />
-    <LatestPosts />
-  </div>
+  return (
+    <div>
+      <Hero />
+      <LatestPosts />
+      <div className="mb-24">
+        <SubscriptionForm />
+      </div>
+      <Footer />
+    </div>
+  )
 }
