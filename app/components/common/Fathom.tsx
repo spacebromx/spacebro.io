de499598229a1a56f09379d1914a13e4cd5ab947
Alan C. Alamillo Medina <realconect@gmail.com>
2023-02-16 16:31:35 -0600

    Fix tracking validation

--- app/components/common/Fathom.tsx
+++ app/components/common/Fathom.tsx
@@ -9,7 +9,7 @@ export default function Fathom() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') return
+    if (process.env.NODE_ENV  !== 'production') return
 
     load(process.env.NEXT_PUBLIC_FATHOM_SITE_ID!, {
       includedDomains: ['spacebro.io']
